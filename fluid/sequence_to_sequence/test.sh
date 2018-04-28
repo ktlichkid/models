35eb053605fd9e95775e3fd8e8a4abdf42a532ee
ktlichkid <liqingsheng@baidu.com>
2018-04-28 08:12:20 +0000

    Added two scripts for testing on GPU

---
+++ fluid/sequence_to_sequence/test.sh
@@ -0,0 +1,4 @@
+export GLOG_v=5
+export GLOG_logtostderr=1
+export CUDA_VISIBLE_DEVICE='2'
+python test_beam_search_decode_op.py
---
+++ fluid/sequence_to_sequence/train.sh
@@ -0,0 +1,4 @@
+export GLOG_v=1
+export GLOG_logtostderr=1
+export CUDA_VISIBLE_DEVICE='2'
+python simple_seq2seq.py
