2936af5de98a475ce3477bdc74dca1300bc528f7
ktlichkid <liqingsheng@baidu.com>
2018-06-05 15:08:36 +0800

    Try print shape

--- fluid/sequence_to_sequence/machine_translation.py
+++ fluid/sequence_to_sequence/machine_translation.py
@@ -212,7 +212,7 @@ def seq_to_seq_net(embedding_dim, encoder_size, decoder_size, source_dict_dim,
             hidden_mem = rnn.memory(init=decoder_boot, need_reorder=True)
             cell_mem = rnn.memory(init=cell_init)
             # context = simple_attention(encoder_vec, encoder_proj, hidden_mem)
-            context = fluid.layers.fill_constant(shape=[1,64], value=0.0, dtype='float32')
+            context = fluid.layers.fill_constant(shape=[-1,64], value=0.0, dtype='float32')
             decoder_inputs = fluid.layers.concat(
                 input=[current_word, context], axis=1)
             h, c = lstm_step(decoder_inputs, hidden_mem, cell_mem, decoder_size)
