b32f96e81131674be05010f703d79f9e72a86ecf
ktlichkid <liqingsheng@baidu.com>
2018-06-05 15:37:43 +0800

    reshape

--- fluid/sequence_to_sequence/machine_translation.py
+++ fluid/sequence_to_sequence/machine_translation.py
@@ -213,9 +213,7 @@ def seq_to_seq_net(embedding_dim, encoder_size, decoder_size, source_dict_dim,
             hidden_mem = rnn.memory(init=decoder_boot, need_reorder=True)
             cell_mem = rnn.memory(init=cell_init)
             # context = simple_attention(encoder_vec, encoder_proj, hidden_mem)
-            constext = fluid.layers.fill_constant(shape=[1,64], value=0.0, dtype='float32')
-            fcontext = fluid.layers.reshape(x=constext,shape=[-1])
-            context = fluid.layers.reshape(x=fcontext,shape=[-1,64])
+            context = fluid.layers.fill_constant(shape=current_word.shape, value=0.0, dtype='float32')
             decoder_inputs = fluid.layers.concat(
                 input=[current_word, context], axis=1)
             h, c = lstm_step(decoder_inputs, hidden_mem, cell_mem, decoder_size)
