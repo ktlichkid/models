8f96d13b102a0c8c45c9db2f2c914c1e8560a6c6
ktlichkid <liqingsheng@baidu.com>
2018-06-06 16:45:39 +0800

    clip all

--- fluid/sequence_to_sequence/machine_translation.py
+++ fluid/sequence_to_sequence/machine_translation.py
@@ -101,6 +101,9 @@ parser.add_argument(
 load_first = True
 model_save_dir = "model_attention"
 
+clip = fluid.clip.GradientClipByNorm(clip_norm=1.0)
+clip_attr = fluid.param_attr.ParamAttr(gradient_clip=clip)
+
 def lstm_step(x_t, hidden_t_prev, cell_t_prev, size):
     def linear(inputs):
         return fluid.layers.fc(input=inputs, size=size, bias_attr=True)
@@ -132,17 +135,21 @@ def seq_to_seq_net(embedding_dim, encoder_size, decoder_size, source_dict_dim,
         # So the output size is 4 times of gate_size.
         input_forward_proj = fluid.layers.fc(input=input_seq,
                                              size=gate_size * 4,
-                                             act=None,
+                                             act='tanh',
+                                             param_attr=clip_attr,
                                              bias_attr=False)
         forward, _ = fluid.layers.dynamic_lstm(
-            input=input_forward_proj, size=gate_size * 4, use_peepholes=False)
+            input=input_forward_proj, size=gate_size * 4,
+            param_attr=clip_attr, use_peepholes=False)
         input_reversed_proj = fluid.layers.fc(input=input_seq,
                                               size=gate_size * 4,
-                                              act=None,
+                                              act='tanh',
+                                              param_attr=clip_attr,
                                               bias_attr=False)
         reversed, _ = fluid.layers.dynamic_lstm(
             input=input_reversed_proj,
             size=gate_size * 4,
+            param_attr=clip_attr,
             is_reverse=True,
             use_peepholes=False)
         return forward, reversed
@@ -190,14 +197,12 @@ def seq_to_seq_net(embedding_dim, encoder_size, decoder_size, source_dict_dim,
                                                bias_attr=False)
             attention_weights = fluid.layers.sequence_softmax(
                input=attention_weights)
-            attention_weights = fluid.layers.Print(attention_weights)
 
             weigths_reshape = fluid.layers.reshape(
                x=attention_weights, shape=[-1])
             scaled = fluid.layers.elementwise_mul(
                x=encoder_vec, y=weigths_reshape, axis=0)
             context = fluid.layers.sequence_pool(input=scaled, pool_type='average')
-            #context = fluid.layers.sequence_pool(input=encoder_vec, pool_type='sum')
             return context
 
         rnn = fluid.layers.DynamicRNN()
@@ -211,15 +216,13 @@ def seq_to_seq_net(embedding_dim, encoder_size, decoder_size, source_dict_dim,
 
         with rnn.block():
             current_word = rnn.step_input(target_embedding)
-            # current_word = fluid.layers.Print(current_word)
             encoder_vec = rnn.static_input(encoder_vec)
             encoder_proj = rnn.static_input(encoder_proj)
             hidden_mem = rnn.memory(init=decoder_boot, need_reorder=True)
             cell_mem = rnn.memory(init=cell_init)
             context = simple_attention(encoder_vec, encoder_proj, hidden_mem)
-            #context = fluid.layers.sequence_pool(encoder_vec, pool_type='first')
             decoder_inputs = fluid.layers.concat(
-                input=[current_word, context], axis=1)
+                input=[context, current_word], axis=1)
             h, c = lstm_step(decoder_inputs, hidden_mem, cell_mem, decoder_size)
             rnn.update_memory(hidden_mem, h)
             rnn.update_memory(cell_mem, c)
