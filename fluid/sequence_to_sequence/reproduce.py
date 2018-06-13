8144144ff7b85fea1f454d9360e6f5858511063c
ktlichkid <liqingsheng@baidu.com>
2018-06-13 12:23:54 +0800

    remove loop

--- fluid/sequence_to_sequence/reproduce.py
+++ fluid/sequence_to_sequence/reproduce.py
@@ -61,39 +61,29 @@ def seq_to_seq_net(embedding_dim, encoder_size, decoder_size, source_dict_dim,
     decoder_boot = fluid.layers.sequence_pool(
         input=encoded_proj, pool_type='last')
 
-    rnn = fluid.layers.DynamicRNN()
+    decoder_state_proj = decoder_boot
+    decoder_state_proj = fluid.layers.Print(
+        decoder_state_proj, message="decoder_state_proj", summarize=10)
+    decoder_state_expand = fluid.layers.sequence_expand(
+       x=decoder_state_proj, y=encoded_proj)
+    decoder_state_expand = fluid.layers.Print(
+        decoder_state_expand, message="decoder_state_expand", summarize=10)
 
-    with rnn.block():
-        current_word = rnn.step_input(trg_embedding)
-        encoder_proj = rnn.static_input(encoded_proj)
-        hidden_mem = rnn.memory(init=decoder_boot)
+    concated = fluid.layers.concat(
+      input=[encoded_proj, decoder_state_expand], axis=1)
+    concated = fluid.layers.Print(
+       concated, message="concated", summarize=10)
+    context = fluid.layers.sequence_pool(input=concated, pool_type='sum')
+    decoder_inputs = fluid.layers.concat(
+        input=[context, trg_embedding], axis=1)
+    h = fluid.layers.fc(input=[decoder_boot, decoder_inputs], size=decoder_size, bias_attr=True)
 
-        decoder_state_proj = hidden_mem
-        decoder_state_proj = fluid.layers.Print(
-            decoder_state_proj, message="decoder_state_proj", summarize=10)
-        decoder_state_expand = fluid.layers.sequence_expand(
-           x=decoder_state_proj, y=encoder_proj)
-        decoder_state_expand = fluid.layers.Print(
-            decoder_state_expand, message="decoder_state_expand", summarize=10)
+    out = fluid.layers.fc(input=h,
+                          size=target_dict_dim,
+                          bias_attr=True,
+                          act='softmax')
 
-        concated = decoder_state_expand
-          #   fluid.layers.concat(
-          # input=[encoder_proj, decoder_state_expand], axis=1)
-        concated = fluid.layers.Print(
-           concated, message="concated", summarize=10)
-        context = fluid.layers.sequence_pool(input=concated, pool_type='sum')
-        decoder_inputs = fluid.layers.concat(
-            input=[context, current_word], axis=1)
-        h = fluid.layers.fc(input=[hidden_mem, decoder_inputs], size=decoder_size, bias_attr=True)
-
-        rnn.update_memory(hidden_mem, h)
-        out = fluid.layers.fc(input=h,
-                              size=target_dict_dim,
-                              bias_attr=True,
-                              act='softmax')
-        rnn.output(out)
-
-    prediction = rnn()
+    prediction = out
 
     prediction = fluid.layers.Print(prediction, message="prediction", summarize=10)
     cost = fluid.layers.cross_entropy(input=prediction, label=label)
