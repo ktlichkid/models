a5b0d878d502e1cc1a0983285c81d5bef3920828
ktlichkid <liqingsheng@baidu.com>
2018-06-14 17:53:56 +0800

    while loop added

--- fluid/sequence_to_sequence/reproduce.py
+++ fluid/sequence_to_sequence/reproduce.py
@@ -42,44 +42,42 @@ def train():
 
     src_word_idx = fluid.layers.data(
         name='source_sequence', shape=[1], dtype='int64', lod_level=1)
+
     src_embedding = fluid.layers.embedding(
         input=src_word_idx,
         size=[30000, 32],
         dtype='float32')
-#    src_embdingding = fluid.layers.Print(src_embedding, message="src", summarize=10)
 
     encoded_proj = fluid.layers.fc(input=src_embedding,
                                    size=32,
                                    bias_attr=False)
-#    encoded_proj = fluid.layers.Print(encoded_proj, message="encoded_proj", summarize=10)
-
 
     decoder_state_proj = fluid.layers.sequence_pool(
         input=encoded_proj, pool_type='last')
-#    decoder_state_proj = fluid.layers.Print(decoder_state_proj, message="proj")
 
     decoder_state_expand = fluid.layers.sequence_expand(
        x=decoder_state_proj, y=encoded_proj)
-#    print(decoder_state_expand.name)
-#    decoder_state_expand = fluid.layers.Print(decoder_state_expand, message="expand")
-    #print(decoder_state_expanded.name)
 
-    decoder_state_concated = fluid.layers.concat(
-        input=[encoded_proj, decoder_state_expand], axis=1)
-#    decoder_state_concated = fluid.layers.Print(
-#        decoder_state_concated, message="concated", summarize=10)
+    counter = fluid.layers.zeros(shape=[1], dtype='int64')
+    counter.stop_gradient = True
+    cond = fluid.layers.less_than(
+        x=counter,
+        y=fluid.layers.fill_constant(
+            shape=[1], dtype='int64', value=2))
+    while_op = fluid.layers.While(cond)
 
-    decoder_repro = fluid.layers.fc(input=decoder_state_concated,
-                                    size=32,
-                                    bias_attr=True,
-                                    act='tanh')
-    decoder_repro = fluid.layers.Print(decoder_repro)
+    with while_op.block():
+        yield
+        decoder_state_expand = fluid.layers.fc(
+            input=decoder_state_expand,
+            size=32,
+            bias_attr=False,
+            act=None)
 
-    prediction = fluid.layers.fc(input=decoder_repro,
+    prediction = fluid.layers.fc(input=decoder_state_expand,
                           size=30000,
                           bias_attr=True,
                           act='softmax')
-#    prediction = fluid.layers.Print(prediction, message="prediction", summarize=10)
 
     cost = fluid.layers.cross_entropy(input=prediction, label=src_word_idx)
     avg_cost = fluid.layers.mean(x=cost)
@@ -104,7 +102,6 @@ def train():
     lod_t.set(np.array(data), place)
     lod_t.set_lod([lod])
 
-#    place = core.CPUPlace() if args.device == 'CPU' else core.CUDAPlace(0)
     exe = Executor(place)
     exe.run(framework.default_startup_program())
 
@@ -112,15 +109,12 @@ def train():
 
     for i in range(0, 2):
         for batch_id, data in enumerate(train_batch_generator()):
-            src_seq, word_num = to_lodtensor(map(lambda x: x[0], data), place)
 
             fetch_outs = exe.run(framework.default_main_program(),
                                  feed={
                                      feeding_list[0]: lod_t
                                  },
                                  fetch_list=[avg_cost])
-                                             #decoder_state_expand.name+"@GRAD"])
-                                             #decoder_state_expand.name+"@GRAD"])
             for out in fetch_outs:
                 print(out)
 
