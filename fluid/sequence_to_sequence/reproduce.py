f6b6446560022e365661f57b550c0ad3b59c818a
ktlichkid <liqingsheng@baidu.com>
2018-06-14 08:03:37 +0000

    reproduce

--- fluid/sequence_to_sequence/reproduce.py
+++ fluid/sequence_to_sequence/reproduce.py
@@ -46,22 +46,29 @@ def train():
         input=src_word_idx,
         size=[30000, 32],
         dtype='float32')
+#    src_embdingding = fluid.layers.Print(src_embedding, message="src", summarize=10)
 
     encoded_proj = fluid.layers.fc(input=src_embedding,
                                    size=32,
                                    bias_attr=False)
+#    encoded_proj = fluid.layers.Print(encoded_proj, message="encoded_proj", summarize=10)
+
 
     decoder_state_proj = fluid.layers.sequence_pool(
         input=encoded_proj, pool_type='last')
+#    decoder_state_proj = fluid.layers.Print(decoder_state, message="proj")
 
     decoder_state_expand = fluid.layers.sequence_expand(
        x=decoder_state_proj, y=encoded_proj)
-#    decoder_state_expand = fluid.layers.Print(decoder_state_expand)
+#    print(decoder_state_expand.name)
+    #decoder_state_expanded = fluid.layers.Print(decoder_state_expand, message="expand")
+    #print(decoder_state_expanded.name)
 
     prediction = fluid.layers.fc(input=decoder_state_expand,
                           size=30000,
                           bias_attr=True,
                           act='softmax')
+#    prediction = fluid.layers.Print(prediction, message="prediction", summarize=10)
 
     cost = fluid.layers.cross_entropy(input=prediction, label=src_word_idx)
     avg_cost = fluid.layers.mean(x=cost)
@@ -76,7 +83,7 @@ def train():
     train_batch_generator = paddle.batch(
         paddle.reader.shuffle(
             wmt14.train(30000), buf_size=1000),
-        batch_size=16)
+        batch_size=1)
 
     place = core.CPUPlace() if args.device == 'CPU' else core.CUDAPlace(0)
 
@@ -90,6 +97,8 @@ def train():
     exe = Executor(place)
     exe.run(framework.default_startup_program())
 
+    #print(framework.default_main_program())
+
     for batch_id, data in enumerate(train_batch_generator()):
         src_seq, word_num = to_lodtensor(map(lambda x: x[0], data), place)
 
@@ -97,11 +106,11 @@ def train():
                              feed={
                                  feeding_list[0]: lod_t
                              },
-                             fetch_list=[avg_cost, prediction.name+"@GRAD",
-                                         decoder_state_expand.name+"@GRAD",
-                                         decoder_state_proj.name+"@GRAD",
-                                         encoded_proj.name+"@GRAD"])
-        print(fetch_outs)
+                             fetch_list=[avg_cost])
+                                         #decoder_state_expand.name+"@GRAD"])
+                                         #decoder_state_expand.name+"@GRAD"])
+        for out in fetch_outs:
+            print(out)
 
 
 if __name__ == '__main__':
