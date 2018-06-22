7aa0b51803eafdf0886145c6e976d5e3f43013b2
ktlichkid <liqingsheng@baidu.com>
2018-06-22 17:42:01 +0800

    rename pd to layers

--- fluid/neural_machine_translation/rnn_search/no_attention_model.py
+++ fluid/neural_machine_translation/rnn_search/no_attention_model.py
@@ -17,7 +17,7 @@ import paddle
 import paddle.fluid as fluid
 import paddle.fluid.core as core
 import paddle.fluid.framework as framework
-import paddle.fluid.layers as pd
+import paddle.fluid.layers as layers
 from paddle.fluid.executor import Executor
 from beam_search_api import *
 import os
@@ -43,19 +43,19 @@ model_save_dir = 'model_wmt14'
 
 def encoder():
     # encoder
-    src_word_id = pd.data(
+    src_word_id = layers.data(
         name="src_word_id", shape=[1], dtype='int64', lod_level=1)
-    src_embedding = pd.embedding(
+    src_embedding = layers.embedding(
         input=src_word_id,
         size=[dict_size, word_dim],
         dtype='float32',
         is_sparse=IS_SPARSE,
         param_attr=fluid.ParamAttr(name='vemb'))
 
-    fc1 = pd.fc(input=src_embedding, size=hidden_dim * 4, act='tanh')
-    lstm_hidden0, lstm_0 = pd.dynamic_lstm(input=fc1, size=hidden_dim * 4)
-    encoder_out = pd.sequence_last_step(input=lstm_hidden0)
-#    pd.Print(encoder_out, message="encoder_out",
+    fc1 = layers.fc(input=src_embedding, size=hidden_dim * 4, act='tanh')
+    lstm_hidden0, lstm_0 = layers.dynamic_lstm(input=fc1, size=hidden_dim * 4)
+    encoder_out = layers.sequence_last_step(input=lstm_hidden0)
+#    layers.Print(encoder_out, message="encoder_out",
 #             print_tensor_name=False,
 #             print_tensor_shape=False, print_tensor_lod=False)
     return encoder_out
@@ -71,7 +71,7 @@ def decoder_state_cell(context):
         current_word = state_cell.get_input('x')
         prev_h = state_cell.get_state('h')
         # make sure lod of h heritted from prev_h
-        h = pd.fc(input=[prev_h, current_word], size=decoder_size, act='tanh')
+        h = layers.fc(input=[prev_h, current_word], size=decoder_size, act='tanh')
         state_cell.set_state('h', h)
 
     return state_cell
@@ -79,9 +79,9 @@ def decoder_state_cell(context):
 
 def decoder_train(state_cell):
     # decoder
-    trg_language_word = pd.data(
+    trg_language_word = layers.data(
         name="target_language_word", shape=[1], dtype='int64', lod_level=1)
-    trg_embedding = pd.embedding(
+    trg_embedding = layers.embedding(
         input=trg_language_word,
         size=[dict_size, word_dim],
         dtype='float32',
@@ -93,9 +93,9 @@ def decoder_train(state_cell):
     with decoder.block():
         current_word = decoder.step_input(trg_embedding)
         decoder.state_cell.compute_state(inputs={'x': current_word})
-        current_score = pd.fc(input=decoder.state_cell.get_state('h'),
-                              size=target_dict_dim,
-                              act='softmax')
+        current_score = layers.fc(input=decoder.state_cell.get_state('h'),
+                                  size=target_dict_dim,
+                                  act='softmax')
         decoder.state_cell.update_states()
         decoder.output(current_score)
 
@@ -103,12 +103,12 @@ def decoder_train(state_cell):
 
 
 def decoder_decode(state_cell):
-    init_ids = pd.data(name="init_ids", shape=[1], dtype="int64", lod_level=2)
-    init_scores = pd.data(
+    init_ids = layers.data(name="init_ids", shape=[1], dtype="int64", lod_level=2)
+    init_scores = layers.data(
         name="init_scores", shape=[1], dtype="float32", lod_level=2)
 
     def embedding(input):
-        return pd.embedding(
+        return layers.embedding(
             input=input,
             size=[dict_size, word_dim],
             dtype='float32',
@@ -122,24 +122,24 @@ def decoder_decode(state_cell):
         prev_scores = decoder.read_array(init=init_scores, is_scores=True)
         prev_ids_embedding = embedding(prev_ids)
         prev_state = decoder.state_cell.get_state('h')
-        prev_state_expanded = pd.sequence_expand(prev_state, prev_scores)
+        prev_state_expanded = layers.sequence_expand(prev_state, prev_scores)
         decoder.state_cell.set_state('h', prev_state_expanded)
         decoder.state_cell.compute_state(inputs={'x': prev_ids_embedding})
         current_state = decoder.state_cell.get_state('h')
-        current_state_with_lod = pd.lod_reset(x=current_state, y=prev_scores)
-        scores = pd.fc(input=current_state_with_lod,
-                       size=target_dict_dim,
-                       act='softmax')
-        topk_scores, topk_indices = pd.topk(scores, k=50)
-        accu_scores = pd.elementwise_add(
-            x=pd.log(x=pd.softmax(topk_scores)),
-            y=pd.reshape(prev_scores, shape=[-1]),
+        current_state_with_lod = layers.lod_reset(x=current_state, y=prev_scores)
+        scores = layers.fc(input=current_state_with_lod,
+                           size=target_dict_dim,
+                           act='softmax')
+        topk_scores, topk_indices = layers.topk(scores, k=50)
+        accu_scores = layers.elementwise_add(
+            x=layers.log(x=layers.softmax(topk_scores)),
+            y=layers.reshape(prev_scores, shape=[-1]),
             axis=0)
-        selected_ids, selected_scores = pd.beam_search(
+        selected_ids, selected_scores = layers.beam_search(
             prev_ids, topk_indices, accu_scores, beam_size, end_id=1, level=0)
 
-        with pd.Switch() as switch:
-            with switch.case(pd.is_empty(selected_ids)):
+        with layers.Switch() as switch:
+            with switch.case(layers.is_empty(selected_ids)):
                 decoder.early_stop()
             with switch.default():
                 decoder.state_cell.update_states()
@@ -178,10 +178,10 @@ def train_main():
     context = encoder()
     state_cell = decoder_state_cell(context)
     rnn_out = decoder_train(state_cell)
-    label = pd.data(
+    label = layers.data(
         name="target_language_next_word", shape=[1], dtype='int64', lod_level=1)
-    cost = pd.cross_entropy(input=rnn_out, label=label)
-    avg_cost = pd.mean(x=cost)
+    cost = layers.cross_entropy(input=rnn_out, label=label)
+    avg_cost = layers.mean(x=cost)
 
     optimizer = fluid.optimizer.Adagrad(learning_rate=1e-3)
     optimizer.minimize(avg_cost)
